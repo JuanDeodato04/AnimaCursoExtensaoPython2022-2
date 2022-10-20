40e0282a717bd7661dff7a1b7f898e2e1fbf7d7b
Juan Deodato <jdeodato88@gmail.com>
2022-10-20 01:04:02 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Meu primeiro projeto Python!
+
+# print() = comando de saída
+print('Hello World!')
+
+# Quando quiser guardar uma String (frase)!
+nome = 'Juan Deodato'
+
+# Para guardar um número inteiro em uma variável
+idade = 20
+
+# Exibir o meu nome (que está dentro da variável nome)
+print(nome)
+
+# Quando quiser exibir a frase "Minha idade é " completando com o conteúdo da variável idade
+print('Minha idade é ' + str(idade) + ' anos.')
+print(f'Minha idade é {idade}.')
